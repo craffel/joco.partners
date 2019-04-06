05ab59daa0b818c8398525e0d5fc9e4e0b1e2d44
Jason Spidle <jason@foundrymakes.com>
2019-04-06 02:23:01 -0500

    Move grid animate above build scripts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-const grid = document.querySelector(".container");
+const grid = document.querySelector('.container');
 
 animateCSSGrid.wrapGrid(grid, {
   duration: 350,
@@ -9,12 +9,12 @@ animateCSSGrid.wrapGrid(grid, {
       console.log(`finished animation for ${elements.length} elements`)
 });
 
-grid.addEventListener("click", ev => {
+grid.addEventListener('click', ev => {
   let target = ev.target;
   console.log(ev.target);
-  while (target.tagName !== "HTML") {
-      if (target.classList.contains("card-grow")) {
-        target.classList.toggle("card-expanded");
+  while (target.tagName !== 'HTML') {
+      if (target.classList.contains('card-grow')) {
+        target.classList.toggle('card-expanded');
         return;
       }
     target = target.parentElement;
