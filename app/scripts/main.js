421f5e3058e497d3937c0cb3db974b124031c332
Jason Spidle <jason@foundrymakes.com>
2019-04-06 02:16:41 -0500

    Initial commit.

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+const grid = document.querySelector(".container");
+
+animateCSSGrid.wrapGrid(grid, {
+  duration: 350,
+  stagger: 10,
+  onStart: elements =>
+      console.log(`started animation for ${elements.length} elements`),
+  onEnd: elements =>
+      console.log(`finished animation for ${elements.length} elements`)
+});
+
+grid.addEventListener("click", ev => {
+  let target = ev.target;
+  console.log(ev.target);
+  while (target.tagName !== "HTML") {
+      if (target.classList.contains("card-grow")) {
+        target.classList.toggle("card-expanded");
+        return;
+      }
+    target = target.parentElement;
+  }
+});
