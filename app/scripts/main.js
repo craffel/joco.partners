717d1cb5a429b4972c86ed342000b1f604207818
Jason Spidle <jason@foundrymakes.com>
2019-04-21 00:01:09 -0500

    Simplify grid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,20 @@
 ** SELECT 15 RANDOM PHOTOS
 */
 let photoArray = []
-while(photoArray.length < 15){
+while(photoArray.length < 12){
     let r = Math.floor(Math.random()*171) + 1;
     if(photoArray.indexOf(r) === -1) photoArray.push(r);
 }
 
 photoArray.forEach(function(photoNumber, i) {
-  let newPhoto = '<div class="card-slideshow-slide" style="background-image: url(../images/' + photoNumber + '.jpg)"></div>';
+  let newPhoto = '<div class="slideshow-image crossfade-animation" style="background-image: url(../images/' + photoNumber + '.jpg)"></div>';
   let newEl = document.createElement('div');
 
   newEl.innerHTML = newPhoto;
-  newEl.className = 'card-slideshow-container';
+  newEl.className = 'slideshow-container';
 
   while(newEl.firstChild) {
-    document.getElementById('content-slideshow').appendChild(newEl.firstChild);
+    document.getElementById('slideshow').appendChild(newEl.firstChild);
   }
 });
 
@@ -64,14 +64,118 @@ const websiteContent = ['headline', 'invitation', 'schedule1', 'schedule2', 'sch
 base('Website Content').select({
     view: 'Grid view'
 }).eachPage(function page(records, fetchNextPage) {
+    document.getElementById('invitation').innerHTML =
+      '<h1>' +
+      records[0].fields.Heading +
+      '</h1><p>' +
+      records[0].fields.Body +
+      '</p>'
+;
+    document.getElementById('sched-one').innerHTML =
+      '<h2 class="schedule">' +
+      records[1].fields.Heading +
+      '</h2><h3>' +
+      records[1].fields.Body +
+      '</h3>'
+;
+    document.getElementById('sched-two').innerHTML =
+      '<h2 class="schedule">' +
+      records[2].fields.Heading +
+      '</h2><h3>' +
+      records[2].fields.Body +
+      '</h3>'
+;
+    document.getElementById('sched-three').innerHTML =
+      '<h2 class="schedule">' +
+      records[3].fields.Heading +
+      '</h2><h3>' +
+      records[3].fields.Body +
+      '</h3>'
+;
+    document.getElementById('sched-four').innerHTML =
+      '<h2 class="schedule">' +
+      records[4].fields.Heading +
+      '</h2><h3>' +
+      records[4].fields.Body +
+      '</h3>'
+;
+    document.getElementById('rsvp').innerHTML =
+      '<h2>' +
+      records[5].fields.Heading +
+      '</h2><p>' +
+      records[5].fields.Body +
+      '</p>'
+;
+    document.getElementById('travel').innerHTML =
+      '<h2>' +
+      records[6].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[6].fields.Body +
+      '</div>';
+
+    document.getElementById('accommodations').innerHTML =
+      '<h2>' +
+      records[7].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[7].fields.Body +
+      '</div>';
+
+    document.getElementById('todo').innerHTML =
+      '<h2>' +
+      records[8].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[8].fields.Body +
+      '</div>';
+
+      document.getElementById('contact').innerHTML =
+        '<h2>' +
+      records[9].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[9].fields.Body +
+      '</div>'
+;
+    document.getElementById('faq').innerHTML =
+      '<h2>' +
+      records[10].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[10].fields.Body +
+      '</div>';
+
+    document.getElementById('contribute').innerHTML =
+      '<h2>' +
+      records[11].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[11].fields.Body +
+      '</div>';
+
+    document.getElementById('requests').innerHTML =
+      '<h2>' +
+      records[12].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[12].fields.Body +
+      '</div>';
+
+    document.getElementById('secret').innerHTML =
+      '<h2>' +
+      records[13].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[13].fields.Body +
+      '</div>';
+
+    document.getElementById('draw').innerHTML =
+      '<h2>' +
+      records[14].fields.Heading +
+      '</h2><div class="body-hidden">' +
+      records[14].fields.Body +
+      '</div>';
+
+    // records.forEach(function(record, i) {
+    //   document.getElementById('content-' + websiteContent[i]).innerHTML =
+    //     '<div><h2>' + record.get('Name') + '</h2>'
+    //     + record.get('Content') + '</div>';
+    // });
+
     // This function (`page`) will get called for each page of records.
-
-    records.forEach(function(record, i) {
-      document.getElementById('content-' + websiteContent[i]).innerHTML =
-        '<div><h2>' + record.get('Name') + '</h2>'
-        + record.get('Content') + '</div>';
-    });
-
     fetchNextPage();
 
 }, function done(err) {
